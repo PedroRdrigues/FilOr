edbeea24b84ec37d91ba7be836df17fe05d60003
ZorcKs <p.hrdrigues1@gmail.com>
2024-02-24 02:27:33 -0300

    Redefinição geral da janela principal

--- main.py
+++ main.py
@@ -3,35 +3,22 @@ from tkinter import Tk
 
 root = Tk()
 
+# criar um função para fazer a troca de cores de acordo com o tema.
 
+# Classe Mãe para a criação de janelas
 class App():
     def __init__(self) -> None:
         self.root = root
-        self.window()
-        self.frames()
-        self.widgets_Lframe()
+        self.window_base()
         root.mainloop()
     
-    def window(self):
+    # Função de criação de janela base
+    def window_base(self) -> None:
         self.root.title('FilOr')
-        self.root.geometry('800x500')
+        self.root.geometry('300x500')
         self.root.configure(background='#131f38')
-        self.root.maxsize(950,650)
-        self.root.minsize(650,350)
-    
-    def frames(self):
-        self.Lframe = Frame(self.root, bd=4, bg='#dfe3ee',
-                            highlightbackground='#759de6', highlightthickness=1.8)
-        self.Lframe.place(relx=0.01,rely=0.01,relwidth=0.4,relheight=0.98)
+        self.root.resizable(False,False)
         
-        self.Rframe = Frame(self.root, bd=4, bg='#dfe3ee',
-                            highlightbackground='#759de6', highlightthickness=1.8)
-        self.Rframe.place(relx=0.414,rely=0.01,relwidth=0.576,relheight=0.98)
     
-    def widgets_Lframe(self): ...
-
-
-
-
 
 App()
