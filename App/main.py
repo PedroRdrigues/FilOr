0943e969a0380dc7ec9f89f55ce86114c7dcd8af
ZorcKs <p.hrdrigues1@gmail.com>
2024-03-06 01:10:13 -0300

    Alterações feitas no README e main.py. Reorganização do arquivo file_oganizer.py .

---
+++ App/main.py
@@ -0,0 +1,28 @@
+from tkinter import *
+from tkinter import Tk
+
+from Funcs.file_organizer import fileOrganizer
+
+root = Tk()
+
+# criar um função para fazer a troca de cores de acordo com o tema.
+
+# Classe Mãe para a criação de janelas
+class App():
+  def __init__(self) -> None:
+    self.root = root
+    self.window_base()
+    root.mainloop()
+
+  # Função de criação de janela base
+  def window_base(self) -> None:
+    self.root.title('FilOr')
+    self.root.geometry('300x500')
+    self.root.configure(background='#131f38')
+    self.root.resizable(False,False)
+
+  def widgets(self):
+    ...
+
+
+App()
